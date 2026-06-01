bb0c37d92d7b0a0a7895bac9d0ecdece69d6732d
jcqm <64622501+jcqm@users.noreply.github.com>
2026-05-31 23:47:05 -0500

    Agregue "Agregar productos"

--- app/src/main/java/com/sise/mibodega/ui/dashboard_fragments/Stock.kt
+++ app/src/main/java/com/sise/mibodega/ui/dashboard_fragments/Stock.kt
@@ -1,26 +1,45 @@
 package com.sise.mibodega.ui.dashboard_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.sise.mibodega.R
+import com.sise.mibodega.ui.DashboardActivity
 
 class Stock : Fragment() {
 
+    private lateinit var btnAgregarProducto: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        arguments?.let {}
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_stock, container, false)
+
+
+        btnAgregarProducto = view.findViewById(R.id.btnAgregarProducto)
+
+        btnAgregarProducto.setOnClickListener {
+
+            val intent = Intent(
+                requireActivity(),
+                com.sise.mibodega.ui.AgregarProducto::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        return view
     }
+
 
 }
