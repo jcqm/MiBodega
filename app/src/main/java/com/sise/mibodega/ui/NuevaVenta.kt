230e6d7529604c9b9819d083b7219b20c55f74de
jcqm <64622501+jcqm@users.noreply.github.com>
2026-06-08 00:05:00 -0500

    Empezando a hacerr la nueva venta

---
+++ app/src/main/java/com/sise/mibodega/ui/NuevaVenta.kt
@@ -0,0 +1,18 @@
+package com.sise.mibodega.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sise.mibodega.R
+
+class NuevaVenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nueva_venta)
+
+
+
+    }
+}
