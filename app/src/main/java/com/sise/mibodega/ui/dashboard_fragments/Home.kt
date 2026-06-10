54b12d6f0d9b1e0884cf3558cd50fc17a3f4e82c
jcqm <64622501+jcqm@users.noreply.github.com>
2026-06-10 01:00:02 -0500

    Tratando de crear nuevo producto como si fuera un carrito

--- app/src/main/java/com/sise/mibodega/ui/dashboard_fragments/Home.kt
+++ app/src/main/java/com/sise/mibodega/ui/dashboard_fragments/Home.kt
@@ -1,9 +1,12 @@
 package com.sise.mibodega.ui.dashboard_fragments
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.sise.mibodega.R
 import  com.sise.mibodega.data.DBHelper
@@ -13,11 +16,15 @@ class Home : Fragment() {
     private lateinit var dbHelper: DBHelper
     private lateinit var saludo: TextView
 
+    private lateinit var btnNuevaVenta: Button
+    private lateinit var btnAgregarProducto: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,6 +36,7 @@ class Home : Fragment() {
         // inicializar
         saludo = view.findViewById(R.id.tvSaludo)
         dbHelper = DBHelper(requireContext(), null)
+        btnNuevaVenta = view.findViewById<Button>(R.id.btnNuevaVenta)
 
         // obtener datos
         val cursor = dbHelper.mostrarNombre()
@@ -41,9 +49,22 @@ class Home : Fragment() {
 
         cursor.close()
 
+
+        /// BOTON NUEVA VENTA //
+
+        btnNuevaVenta.setOnClickListener {
+
+            val intent = Intent(
+                requireActivity(), com.sise.mibodega.ui.NuevaVenta::class.java
+            )
+
+            startActivity(intent)
+        }
+
+
+
         return view
     }
-
 
 
 }
